b5f5808efe4741f58902d02888decb37ccab7116
Riekelt Kapitein <riekelt@riekelt.com>
2020-08-18 09:14:08 +0200

    wip

---
+++ test/Unit/RouteExtensionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017-2020 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/php-library-template
+ */
+
+namespace Iterica\NavigationBundle\Test\Unit;
+
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Iterica\NavigationBundle\Extension\RouteExtension
+ */
+final class RouteExtensionTest extends Framework\TestCase
+{
+    public function testRoutingExtension(): void
+    {
+        self::assertEquals(true, true);
+    }
+}
